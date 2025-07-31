6d2e4225698a9fe6ef05858048325819a89bb8d4
AlexJosua <130466775+AlexJosua@users.noreply.github.com>
2025-07-31 15:52:35 +0700

    feat(order):order data, order list, create order, handle reservation, detail order, order summary

--- src/constants/order-constants.ts
+++ src/constants/order-constants.ts
@@ -1,8 +1,43 @@
 export const HEADER_TABLE_ORDER = [
   "NO",
-  "Orde ID",
+  "Order ID",
   "Customer Name",
   "Table",
+  "Status",
+  "action",
+];
+
+export const INITIAL_ORDER = {
+  customer_name: "",
+  table_id: "",
+  status: "",
+};
+
+export const INITIAL_STATE_ORDER = {
+  status: "idle",
+  errors: {
+    customer_name: [],
+    table_id: [],
+    status: [],
+    _form: [],
+  },
+};
+
+export const STATUS_CREATE_ORDER = [
+  {
+    value: "reserved",
+    label: "Reserved",
+  },
+  {
+    value: "process",
+    label: "Process",
+  },
+];
+
+export const HEADER_TABLE_DETAIL_ORDER = [
+  "NO",
+  "Menu",
+  "Total",
   "Status",
   "action",
 ];
