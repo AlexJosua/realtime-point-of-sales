5c4189805087507e3603d84571a2afabef281d5f
AlexJosua <130466775+AlexJosua@users.noreply.github.com>
2025-08-14 17:55:08 +0700

    feat(payment): handle success and failed payment

---
+++ src/app/payment/success/page.tsx
@@ -0,0 +1,9 @@
+import Success from "./_components/success";
+
+export const metadata = {
+  title: "Alex Cafe | Payment Success",
+};
+
+export default function SuccessPage() {
+  return <Success />;
+}
